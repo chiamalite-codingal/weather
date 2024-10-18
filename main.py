58e6d5da0c02d9693faa9a26ae56f1cd00274428
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-10-18 17:24:04 +0100

    weather condition

---
+++ main.py
@@ -0,0 +1,6 @@
+def weather():
+    print("the weather is good in",fav)
+    print("the weather is bad in",nonfav)
+fav = input("Enter yor favourite weather:")
+nonfav = input("Enter the weather:")
+weather()
